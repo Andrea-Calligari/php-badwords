eaea8776897c457c2d3868f8105084a105b30b7b
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-05-03 16:26:51 +0200

    ADD:  file index and server

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+   <form action="./server.php" method="POST">
+
+    <textarea name="message" id="message" placeholder="Scrivi qui il tuo messaggio "></textarea>
+    <input type="submit" value="Invia">
+
+
+
+
+
+   </form>
+    
+</body>
+</html>
+<?php
+
+
+
+
+?>
---
+++ server.php
@@ -0,0 +1,16 @@
+<?php
+$message = $_POST('message');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Server</title>
+</head>
+<body>
+    <h1>questo é il tuo messaggio <?php echo $message;?></h1>
+    
+</body>
+</html>
